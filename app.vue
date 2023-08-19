c4443e77b4cd5410bf59497542173e8e238579c3
pntbutter <frallanawsm@gmail.com>
2023-08-19 13:27:51 +0200

    goof vh

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="flex  flex-col  h-screen  bg-slate-800  text-slate-100">
+    <div class="h-screen  bg-slate-800  text-slate-100">
         <header class="flex  items-center  justify-between  p-6  border-b  border-slate-700">
             <HeaderElements />
             <HeaderActions />
         </header>
 
-        <div class="grow  flex  divide-x  divide-slate-700">
+        <div class="flex  divide-x  divide-slate-700">
             <Character name="Red Guard" />
             <Character name="Demolitionist" />
             <Character name="Void Warden" />
